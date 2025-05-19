046595cc3176e5f9cf8b55b4ee4a1cbe7c32e5a8
Niranjan669 <niranjanbalurgi@gmail.com>
2025-05-19 14:11:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
+// server.js
+const express = require('express');
+const connectDB = require('./db');
+const noteRoutes = require('./routes/noteRoutes');
+require('dotenv').config();
 
+const app = express();
+const PORT = 3000;
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(express.json()); // Parse JSON bodies
+
+// Routes
+app.use('/notes', noteRoutes);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
